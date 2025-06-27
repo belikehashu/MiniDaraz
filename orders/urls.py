4393a3c3f4e4c7560afed633d847870af231f0cc
belikehashu <jjwhjwnhj@gmail.com>
2025-06-26 18:35:39 -0700

    errors resolved - update v4

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('checkout/', views.checkout_view, name='checkout'),
     path('history/', views.order_history_view, name='order_history'),
     path('buy/<int:product_id>/', views.buy_now_view, name='buy_now'),
+    path('cancel/<int:order_id>/', views.cancel_order, name='cancel_order'),
+    path('delete-history/<int:order_id>/', views.delete_order_history_entry, name='delete_order_history'),
 ]
